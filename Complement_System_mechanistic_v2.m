b46350f38ddf70ddbfd9bf9987712680386a1d80
Ivan Ramirez Zuniga <ivr3@pitt.edu>
2024-03-06 10:33:17 -0600

    Mechanistic v2 update

---
+++ Complement_System_mechanistic_v2.m
@@ -0,0 +1,146 @@
+function dydt = Complement_System_mechanistic_v2(t, y, params)
+% Equations for the activation dynamics
+% of the initial cascade of the classic CS pathway
+
+% includes c3b, disticntion in C3 convertases between CP,LP and AP
+% includes Asperg conidia and hyphae
+
+% Defining State Variables
+C1 = y(1);
+C2 = y(2);
+C3 = y(3);
+C4 = y(4);
+C5 = y(5);
+C3convCPLP = y(6);
+C3convAP = y(7);
+C5conv = y(8);
+C3a = y(9);
+C5a = y(10);
+C3b = y(11);
+C3H2O = y(12); % C3 hydrolized
+Ac = y(13);   % Aspergillus conidia
+Ah = y(14);   % Aspergillus hyphae
+Am = y(15); % Alveolar Macrophages
+N = y(16);  % Neutrophils
+C1inh =y(17) ;
+FB = y(18); % Factor B
+FH = y(19); % Factor H
+FI = y(20); % Factor I
+P = y(21);  % Properdin
+
+
+C3conv = C3convCPLP + C3convAP;
+A = Ac + Ah ;
+
+% Defining model parameters
+
+Kc1 = params(1);           Kc2 = params(2);          Kc3 = params(3);
+Kc4 = params(4);           Kc5 = params(5);          sc1 = params(6);
+sc2 = params(7);           sc3 = params(8);          sc4 = params(9);
+sc5 = params(10);          muc1 = params(11);        muc2 = params(12);
+muc3 = params(13);         muc4 = params(14);        muc5 = params(15);
+C1star = params(16);       C2star = params(17);      C3star = params(18);
+C4star = params(19);       C5star = params(20);      Kc3convcpcl = params(21);
+Kcp = params(22);          Klp = params(23);         C1inhstar = params(24);
+muc3convcplp = params(25); Kc3convap = params(26);   C3H2Ostar = params(27);
+muc3convap = params(28);   Kc5conv = params(29);     Kc5convhs = params(30);
+muc5conv = params(31);     Kc3acplp = params(32);    Kc3aap = params(33);
+muc3a = params(34);        Kc5a = params(35);        muc5a = params(36);
+Kc3bcplp = params(37);     Kc3bh2ofb = params(38);   Kc3bap = params(39);
+FHstar = params(40);       FIstar = params(41);      muc3b = params(42);
+Kc3h2o = params(43);       muc3h2o = params(44);     KAc = params(45);
+Acmax = params(46);        dacm = params(47);        dacn = params(48);
+KAh = params(49);          Ahmax = params(50);       dahm = params(51);
+dahn = params(52);         Kam = params(53);         Kamc3a = params(54);
+Kamc5a = params(55);       Amhs = params(56);        muam = params(57);
+dam = params(58);          Kn = params(59);          Knc3a = params(60);
+Knc5a = params(61);        Nhs = params(62);         mun = params(63);
+                                                     dan = params(64);
+
+sc1inh = params(65);
+muc1inh = params(66);
+sfb = params(67);
+mufb = params(68);
+sfh = params(69);
+mufh = params(70);
+sfi = params(71);
+mufi = params(72);
+Kp =params(73);
+mup = params(74);
+Pstar =params(75);
+
+% Differential Equations
+
+dC1_dt = sc1 + Kc1 * C1 * A/(1+C1/C1star) - muc1 * C1;
+dC2_dt = sc2 + Kc2 * C2 * A/(1+C2/C2star) - muc2 * C2;
+dC3_dt = sc3 + Kc3 * C3 * A/(1+C3/C3star) - muc3 * C3;
+dC4_dt = sc4 + Kc4 * C4 * A/(1+C4/C4star) - muc4 * C4;
+dC5_dt = sc5 + Kc5 * C5 * A/(1+C5/C5star) - muc5 * C5;
+
+% C3 convertase from Classical Pathway (CP) and Lectin Pathway (LP)
+dC3convCPLP_dt = Kc3convcpcl * A *( ( Kcp * C1 * C2 * C4) + (Klp * C2 *  C4) )/( 1 + C1inh/C1inhstar ) -  muc3convcplp * C3convCPLP;
+
+% C3 convertase frrom Alternative Pathway (AP)
+% dC3convAP_dt = Kc3convap * A *( C3H2O/(1+C3H2O/C3H2Ostar) ) -  muc3convap * C3convAP;
+dC3convAP_dt = Kc3convap  * ( C3H2O * FB )/(1+FH/FHstar) -  muc3convap * C3convAP/(1+P/Pstar)*(1+FH);
+
+% C5 convertase
+dC5conv_dt = Kc5conv * ( C3b * C3conv)/( Kc5convhs + ( C3b * C3conv))  -  muc5conv * C5conv;
+
+dC3a_dt = Kc3acplp *  C3 * C3convCPLP + Kc3aap * C3 * C3convAP  - muc3a * C3a;
+
+dC5a_dt = Kc5a *  C5 * C5conv  - muc5a * C5a;
+
+% C3b can be rpoduced from CP-LP an frrom AP
+dC3b_dt = Kc3bcplp * C3 * C3convCPLP + (Kc3bh2ofb * C3H2O * FB  + Kc3bap* C3convAP * C3) / ( (1+FH/FHstar)*(1+FI/FIstar)  ) - muc3b * C3b * (1+FH);
+
+dC3H2O_dt = Kc3h2o * C3 - muc3h2o * C3H2O;
+
+
+
+% Asperg. conidia
+% if Ac<0
+%     Ac=0;
+%     dAc_dt=0;
+% else
+    dAc_dt = KAc * Ac * (1-Ac/Acmax) -dacm  * Ac * Am * C3b - dacn * Ac * N * C3b;
+% end
+
+
+% Asperg. hyphae
+% if Ah<0
+%     Ah=0;
+%     dAh_dt=0;
+% else
+    dAh_dt = KAh * Ac * (1-Ac/Ahmax) - dahm  * Ah * Am * C3b - dahn * Ah * N * C3b;
+% end
+
+% Alveolar macrophages
+if Am<0
+    Am=0;
+    dAm_dt=0;
+else
+    dAm_dt = Kam *A*(Kamc3a * C3a + Kamc5a * C5a )/(Amhs + (Kamc3a * C3a + Kamc5a * C5a )) - muam * (1 + dam * A * C3b)*Am ;
+end
+
+
+% Neutrophils
+if N<0
+    N=0;
+    dN_dt=0;
+else
+    dN_dt = Kn * A *(Knc3a * C3a + Knc5a * C5a)/(Nhs+(Knc3a * C3a + Knc5a * C5a)) - mun *(1 + dan * A * C3b) *N ;
+end
+
+% Inhibitors
+dC1inh_dt = sc1inh - muc1inh * C1inh;
+dFB_dt = sfb - mufb * FB;
+dFH_dt = sfh - mufh * FH;
+dFI_dt = sfi - mufi* FI;
+dP_dt= Kp * N - mup * P;
+
+dydt = [dC1_dt,dC2_dt,dC3_dt,dC4_dt,dC5_dt,...
+    dC3convCPLP_dt,dC3convAP_dt,dC5conv_dt,...
+    dC3a_dt,dC5a_dt,dC3b_dt,dC3H2O_dt,dAc_dt,...
+    dAh_dt,dAm_dt, dN_dt, dC1inh_dt, dFB_dt, dFH_dt,dFI_dt,dP_dt]';
+
---
+++ driver_Complement_System_mechanistic_v2.m
@@ -0,0 +1,327 @@
+clear
+
+% Initial conditions
+C1_0 = 45 * 10^(-2);
+C2_0 = 21.36 * 10^(-2);
+C3_0 = 888.9 * 10^(-2);
+C4_0 = 310.67 * 10^(-2);
+C5_0 = 44.44 * 10^(-2);
+
+C3convCPLP_0 = 0;
+C3convAP_0 = 0;
+
+C5conv_0 = 0;
+C3a_0 = 0;
+C5a_0 = 0;
+C3b_0 = 0;
+C3H2O_0 = 0;
+
+Ac_0=1e2*1 ;
+Ah_0 = 1e1*1;
+
+Am_0 = 0;
+N_0 = 0;
+
+C1inh_0 = 1.7; % (uM according to Korotaevskiy)
+FB_0 = 2.1; % Factor B (uM according to Korotaevskiy)
+FH_0 = 3.2; % Factor H (uM according to Korotaevskiy)
+FI_0 = 0.4; % Factor I (uM according to Korotaevskiy)
+P_0 = 0.42; % Properdin (uM according to Korotaevskiy)
+
+
+% Define parameters
+
+Kc1 = 0.00021;
+Kc2 = 0.0002;
+Kc3 = 0.0005;
+Kc4 = 0.0002;
+Kc5 = 0.0002;
+
+muc1 = 1e-1*1;
+muc2 = 1e-1*1;
+muc3 = 1e-1*1;
+muc4 = 1e-1*1;
+muc5 = 1e-1*1;
+
+sc1 = C1_0*muc1;
+sc2 = C2_0*muc2;
+sc3 = C3_0*muc3;
+sc4 = C4_0*muc4;
+sc5 = C5_0*muc5;
+
+C1star=1e0;
+C2star=1e0;
+C3star=1e0;
+C4star=1e0;
+C5star=1e0;
+
+Kc3convcpcl = 1e-6;
+Kcp = 0.01;
+Klp= 0.2;
+
+C1inhstar = 1;
+muc3convcplp = 1e-1;
+Kc3convap = 1e-3*0.5;
+C3H2Ostar= 1e0;
+muc3convap = 3e-1;
+
+Kc5conv = 1e-1;
+Kc5convhs = 1e1;
+muc5conv = 1e-2;
+
+Kc3acplp = 0.05;
+Kc3aap = 0.02;
+muc3a = 6e-2;
+
+Kc5a = 5e-2;
+muc5a = 1e-2;
+
+Kc3bcplp = 0.05;
+Kc3bh2ofb = 0.03;
+Kc3bap = 0.03;
+
+FHstar = 1;
+FIstar = 1;
+muc3b = 4e-2;
+
+Kc3h2o = 0.002;
+muc3h2o = 5e-2;
+
+KAc = 0.1;
+Acmax = 1e4;
+
+dacm = 5e-2;
+dacn = 1e-3;
+
+KAh = 0.25;
+Ahmax = 1e4;
+
+dahm = 6e-3;
+dahn = 1e-2;
+
+Kam = 0.1;
+Kamc3a = 0.5;
+Kamc5a = 0.5;
+Amhs = 1e2;
+muam = 1e-2*5;
+
+dam = 1e-3;
+Kn = 0.2;
+Knc3a = 0.5;
+Knc5a = 0.5;
+Nhs = 1e2;
+mun = 1e-2;
+dan = 1e-1;
+
+sc1inh = 0.2*0 ;
+muc1inh = 1e-1*0 ;
+sfb = 0.3 *0;
+mufb = 1e-1 *0;
+sfh = 0.3 *0;
+mufh = 1e-2 *0;
+sfi = 0.2 *0;
+mufi = 1e-2 *0;
+
+Kp=1e-2;
+mup=1e-2;
+Pstar=1e1;
+
+
+
+params = [Kc1, Kc2, Kc3, Kc4, Kc5, sc1, sc2, sc3, sc4, sc5, muc1, muc2, muc3, muc4, muc5,...
+    C1star, C2star, C3star, C4star, C5star, Kc3convcpcl, Kcp, Klp, C1inhstar,...
+    muc3convcplp, Kc3convap, C3H2Ostar, muc3convap, Kc5conv, Kc5convhs, muc5conv,...
+    Kc3acplp, Kc3aap, muc3a, Kc5a, muc5a, Kc3bcplp, Kc3bh2ofb, Kc3bap, FHstar,...
+    FIstar, muc3b, Kc3h2o, muc3h2o, KAc, Acmax, dacm, dacn, KAh, Ahmax, dahm, dahn,...
+    Kam, Kamc3a, Kamc5a, Amhs, muam, dam, Kn, Knc3a, Knc5a, Nhs, mun, dan,...
+    sc1inh, muc1inh, sfb, mufb, sfh, mufh, sfi, mufi,Kp,mup,Pstar];
+
+initial_conditions = [C1_0, C2_0, C3_0, C4_0, C5_0, ...
+  C3convCPLP_0, C3convAP_0, C5conv_0, C3a_0,C5a_0,C3b_0, C3H2O_0, Ac_0, ...
+   Ah_0,Am_0, N_0, C1inh_0, FB_0, FH_0, FI_0,P_0];
+
+% Time span
+tspan = [0, 500];
+
+% Call solver
+sol = ode23s(@Complement_System_mechanistic_v2, tspan, initial_conditions, [], params);
+
+% Time points for which to evaluate the solution
+t_eval = (0:0.001:tspan(2));
+
+% Evaluate solution
+Ys = deval(sol, t_eval);
+
+%% Define state variables frrom solutions
+
+C1 = Ys(1, :);
+C2 = Ys(2, :);
+C3 = Ys(3, :);
+C4 = Ys(4, :);
+C5 = Ys(5, :);
+C3convCPLP = Ys(6, :);
+C3convAP =Ys(7,:);
+C5conv = Ys(8, :);
+C3a = Ys(9, :);
+C5a = Ys(10, :);
+C3b = Ys(11, :);
+C3H2O = Ys(12, :);
+Ac = Ys(13, :);
+Ah = Ys(14,:);
+Am = Ys(15,:);
+N = Ys(16,:);
+C1inh =Ys(17,:) ;
+FB = Ys(18,:); % Factor B
+FH = Ys(19,:); % Factor H
+FI = Ys(20,:); % Factor I
+P = Ys(21,:); % Properdin
+
+%%
+% Plot the results
+cols=3;
+rows=2;
+
+linestyle='-';
+
+colors_vec={"#0072BD","#D95319","#EDB120","#7E2F8E","#77AC30","#4DBEEE","#A2142F"};
+
+figure
+subplot(rows,cols,1)
+hold on
+plot(t_eval,C1, 'LineWidth', 2, 'DisplayName', 'C1','LineStyle',linestyle,'Color',colors_vec{1});
+hold on;
+plot(t_eval, C2, 'LineWidth', 2, 'DisplayName', 'C2','LineStyle',linestyle,'Color',colors_vec{3});
+hold on
+plot(t_eval, C3, 'LineWidth', 2, 'DisplayName', 'C3','LineStyle',linestyle,'Color',colors_vec{4});
+hold on
+plot(t_eval, C4, 'LineWidth', 2, 'DisplayName', 'C4','LineStyle',linestyle,'Color',colors_vec{5});
+hold on
+plot(t_eval, C5, 'LineWidth', 2, 'DisplayName', 'C5','LineStyle',linestyle,'Color',colors_vec{6});
+hold on
+plot(t_eval, C3H2O, 'LineWidth', 2, 'DisplayName', 'C3H2O','LineStyle',linestyle,'Color',colors_vec{7});
+xlabel('Time');
+ylabel('Concentration');
+legend('Location', 'northeast');
+% title('Classic Pathway Dynamics');
+grid on;
+
+%%
+subplot(rows,cols,2)
+hold on
+plot(t_eval, C3convCPLP, 'LineWidth', 2, 'DisplayName', 'C3convCPLP','LineStyle',linestyle,'Color',colors_vec{1});
+hold on
+plot(t_eval, C3convAP, 'LineWidth', 2, 'DisplayName', 'C3convAP','LineStyle',linestyle,'Color',colors_vec{2});
+hold on
+plot(t_eval, C5conv, 'LineWidth', 2, 'DisplayName', 'C5conv','LineStyle',linestyle,'Color',colors_vec{3});
+
+xlabel('Time');
+ylabel('Concentration');
+legend('Location', 'Best');
+% title('Classic Pathway Dynamics');
+grid on;
+% ylim([0,10e-12])
+
+%%
+subplot(rows,cols,3)
+hold on
+plot(t_eval, Ac, 'LineWidth', 2, 'DisplayName', 'Asperg. conidia','LineStyle',linestyle,'Color',colors_vec{1});
+hold on
+plot(t_eval, Ah, 'LineWidth', 2, 'DisplayName', 'Asperg. hyphae','LineStyle',linestyle,'Color',colors_vec{2});
+xlabel('Time');
+ylabel('Concentration');
+legend('Location', 'Best');
+% title('Classic Pathway Dynamics');
+grid on;
+
+%%
+subplot(rows,cols,4)
+plot(t_eval, C3a, 'LineWidth', 2, 'DisplayName', 'C3a','LineStyle',linestyle,'Color',colors_vec{1});
+hold on
+plot(t_eval,C5a, 'LineWidth', 2, 'DisplayName', 'C5a','LineStyle',linestyle,'Color',colors_vec{2});
+hold on
+plot(t_eval,C3b, 'LineWidth', 2, 'DisplayName', 'C3b','LineStyle',linestyle,'Color',colors_vec{3});
+xlabel('Time');
+ylabel('Concentration');
+legend('Location', 'Best');
+% title('Classic Pathway Dynamics');
+grid on;
+
+%%
+
+subplot(rows,cols,5)
+hold on
+plot(t_eval, C1inh, 'LineWidth', 2, 'DisplayName', 'C1inh','LineStyle',linestyle,'Color',colors_vec{1});
+hold on
+plot(t_eval, FB, 'LineWidth', 2, 'DisplayName', 'FB','LineStyle',linestyle,'Color',colors_vec{2});
+hold on
+plot(t_eval, FH, 'LineWidth', 2, 'DisplayName', 'FH','LineStyle',linestyle,'Color',colors_vec{3});
+hold on
+plot(t_eval, FI, 'LineWidth', 2, 'DisplayName', 'FI','LineStyle',linestyle,'Color',colors_vec{4});
+hold on
+plot(t_eval, P, 'LineWidth', 2, 'DisplayName', 'P','LineStyle',linestyle,'Color',colors_vec{5});
+xlabel('Time');
+ylabel('Concentration');
+legend('Location', 'Best');
+% title('Classic Pathway Dynamics');
+grid on;
+hold off;
+%%
+
+subplot(rows,cols,6)
+hold on
+plot(t_eval, Am, 'LineWidth', 2, 'DisplayName', 'Am','LineStyle',linestyle,'Color',colors_vec{1});
+hold on
+plot(t_eval, N, 'LineWidth', 2, 'DisplayName', 'N','LineStyle',linestyle,'Color',colors_vec{2});
+hold on
+xlabel('Time');
+ylabel('Concentration');
+legend('Location', 'Best');
+% title('Classic Pathway Dynamics');
+grid on;
+
+
+return
+
+%% 
+figure
+subplot(rows,cols,1)
+hold on
+% plot(t_eval, C4, 'LineWidth', 2, 'DisplayName', 'C4','LineStyle',linestyle,'Color',colors_vec{1});
+hold on
+plot(t_eval, C1, 'LineWidth', 2, 'DisplayName', 'C1','LineStyle',linestyle,'Color',colors_vec{2});
+xlabel('Time');
+ylabel('Concentration');
+legend('Location', 'Best');
+title('Classic Pathway Dynamics');
+grid on;
+%%
+subplot(rows,cols,2)
+hold on
+plot(t_eval, AgAb, 'LineWidth', 2, 'DisplayName', 'AgAb','LineStyle',linestyle,'Color',colors_vec{1});
+xlabel('Time');
+ylabel('Concentration');
+legend('Location', 'Best');
+title('Classic Pathway Dynamics');
+grid on;
+
+subplot(rows,cols,3)
+hold on
+plot(t_eval, C4bC2aC3bC5, 'LineWidth', 2, 'DisplayName', 'C4bC2aC3bC5','LineStyle',linestyle,'Color',colors_vec{1});
+xlabel('Time');
+ylabel('Concentration');
+legend('Location', 'Best');
+title('Classic Pathway Dynamics');
+grid on;
+
+subplot(rows,cols,4)
+hold on
+plot(t_eval, C1bar, 'LineWidth', 2, 'DisplayName', 'C1bar','LineStyle',linestyle,'Color',colors_vec{1});
+xlabel('Time');
+ylabel('Concentration');
+legend('Location', 'Best');
+title('Classic Pathway Dynamics');
+grid on;
+
+
+
+
