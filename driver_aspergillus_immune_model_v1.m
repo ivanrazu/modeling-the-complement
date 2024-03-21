ae7b5b224558a320f61ec3ee499470f0b269c093
Ivan Ramirez Zuniga <ivr3@pitt.edu>
2024-03-20 19:44:17 -0500

    update params

--- driver_aspergillus_immune_model_v1.m
+++ driver_aspergillus_immune_model_v1.m
@@ -73,10 +73,11 @@ mun=mun*2;
 mum=mum*2;
 
 
+
 gamma= 0.5;
 
 
-Ac0=1e0;
+Ac0=1e0*1;
 Ah0=0;
 C5a0=0;
 N0=1e1*0;
@@ -94,7 +95,7 @@ params = [Kac, Acmax,dacm,dacn,Kah,Ahmax,dahn,Kc5a,Aifstar,muc5a,...
 initial_conditions = [Ac0,Ah0,C5a0,N0,M0,R0,Aif0,D0];
 
 % Time span
-tspan = [0, 50];
+tspan = [0, 15];
 
 % Call solver
 sol = ode15s(@aspergillus_immune_model_v1, tspan, initial_conditions, [], params);
@@ -130,8 +131,8 @@ hold on
 plot(t_eval,Ac, 'LineWidth', 2, 'DisplayName', 'Ac','LineStyle',linestyle,'Color',colors_vec{1});
 hold on;
 plot(t_eval,Ah, 'LineWidth', 2, 'DisplayName', 'Ah','LineStyle',linestyle,'Color',colors_vec{2});
-xlim([0,50])
-ylim([0,2])
+% xlim([0,10])
+% ylim([0,2])
 xlabel('Time');
 ylabel('Concentration');
 legend('Location', 'northeast');
@@ -154,6 +155,7 @@ hold on
 plot(t_eval, M, 'LineWidth', 2, 'DisplayName', 'M','LineStyle',linestyle,'Color',colors_vec{1});
 hold on
 plot(t_eval, N, 'LineWidth', 2, 'DisplayName', 'N','LineStyle',linestyle,'Color',colors_vec{2});
+hold on
 xlabel('Time');
 ylabel('Concentration');
 legend('Location', 'Best');
@@ -178,7 +180,7 @@ plot(t_eval, Aif, 'LineWidth', 2, 'DisplayName', 'Aif','LineStyle',linestyle,'Co
 xlabel('Time');
 ylabel('Concentration');
 legend('Location', 'Best');
-ylim([0,10])
+ylim([0,5])
 xlim([0,tspan(end)])
 grid on;
 %%
@@ -191,4 +193,114 @@ legend('Location', 'Best');
 ylim([0,1])
 xlim([0,tspan(end)])
 grid on;
+%%
+
+% % Plots phagocytosis flux
+
+F_aif = 1./(1+(Aif/Aifstar).^2) ;
+phagoc_M=dacm * F_aif  .* Ac .* M .* R./(1+alpha*Ac);
+
+phagoc_N = dacn * F_aif .* Ac .* N .* R./(1+alpha*Ac);
+
+hyphae_killed_by_N =  dahn  * Ah .* N .* R .* F_aif./(1+beta*Ah);
+
+figure
+plot(t_eval,phagoc_M,'LineWidth', 2, 'DisplayName', 'Phagoc M','LineStyle',linestyle,'Color',colors_vec{1})
+hold on
+plot(t_eval,phagoc_N,'LineWidth', 2, 'DisplayName', 'Phagoc M','LineStyle',linestyle,'Color',colors_vec{2})
+hold on
+plot(t_eval,hyphae_killed_by_N,'LineWidth', 2, 'DisplayName', 'Hyphae killed by N','LineStyle',linestyle,'Color',colors_vec{3})
+
+xlabel('Time');
+legend('Location', 'Best');
+grid on;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+%%
+return
+
+Ndat = xlsread('Neutrophils_WT.xlsx','Sheet1', 'A14:E18');
+meanNdat = mean(Ndat);
+sdNdat = std(Ndat);
+
+
+Mdat = xlsread('Monocytes_WT.xlsx','Sheet1', 'A12:E16');
+meanMdat = mean(Mdat);
+sdMdat = std(Mdat);
+
+CXCL2_dat = xlsread('CXCL2_WT.xlsx','Sheet1', 'A12:E16');
+meanCXCL2dat = mean(CXCL2_dat);
+sdCXCL2dat = std(CXCL2_dat);
+
+
+
+
+%%
+tscat = [0,12,24,48,72];
+
+
+figure
+
+subplot(1,3,1)
+errorbar(tscat,meanNdat,sdNdat,'ks','MarkerSize',10,'MarkerFaceColor','black','LineStyle','none','LineWidth',2);
+hold on;
+xlabel('Time (d)');
+ylabel('Log_{10} N');
+xticks([0,12,24,48,72])
+xticklabels({'0','12','24','48','72'})
+grid on
+% xlim([-0.2,12])
+% ylim([3.5,5.5])
+xtickangle(0)
+hold on
+
+
+subplot(1,3,2)
+errorbar(tscat,meanCXCL2dat,sdCXCL2dat,'ks','MarkerSize',10,'MarkerFaceColor','black','LineStyle','none','LineWidth',2);
+hold on;
+xlabel('Time (d)');
+ylabel('Log_{10} CXXL2');
+xticks([0,12,24,48,72])
+xticklabels({'0','12','24','48','72'})
+grid on
+% xlim([-0.2,12])
+% ylim([3.5,5.5])
+xtickangle(0)
+hold on
+
+subplot(1,3,3)
+errorbar(tscat,meanMdat,sdMdat,'ks','MarkerSize',10,'MarkerFaceColor','black','LineStyle','none','LineWidth',2);
+hold on;
+xlabel('Time (d)');
+ylabel('Log_{10} M');
+xticks([0,12,24,48,72])
+xticklabels({'0','12','24','48','72'})
+grid on
+% xlim([-0.2,12])
+% ylim([3.5,5.5])
+xtickangle(0)
+hold on
+%%
+x = (0:0.1:7);
+xstar = 3.5;
+
+fx= 1./(1+(x/xstar).^4);
+
+
+figure
+semilogx(x,fx)
+hold on
+xline(xstar,'r--')
 
